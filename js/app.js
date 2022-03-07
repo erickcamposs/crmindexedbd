2f6547a9b94e66ec778c3a067fb7f7e79041ac3a
Erick Campos <kcire2477@gmail.com>
2022-03-06 18:57:50 -0600

    Primer Commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+(function (){
+    let DB;
+    document.addEventListener('DOMContentLoaded', () => {
+        crearDB();
+    });
+
+    //Crear IndexDB
+    function crearDB(){
+        const clienteDB = window.indexedDB.open('crm', 1);
+        clienteDB.onerror = () => {
+            console.log('Hubo un error');
+        };
+        clienteDB.onsuccess = () => {
+            console.log('Base de datos creada');
+            DB = clienteDB.result;
+        };
+        clienteDB.onupgradeneeded = (e) =>{
+            const db = e.target.result;
+            const objectStore = db.createObjectStore('crm', {
+                keyPath: 'id',
+                autoIncrement: true
+            });
+
+            objectStore.createIndex('nombre', 'nombre', {unique: false});
+            objectStore.createIndex('email', 'email', {unique: true});
+            objectStore.createIndex('telefono', 'telefono', {unique: false});
+            objectStore.createIndex('empresa', 'empresa', {unique: false});
+            objectStore.createIndex('id', 'id', {unique: true});
+        };
+    }
+})();
